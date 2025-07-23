f0a9c710474446234f4f9d64864cd632ed50444c
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-23 17:48:18 +0530

    fetched the nested children by applying recursion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import './App.css'
+import json from "./../data.json";
+import { useState } from 'react';
+
+
+function List({ list }) {
+  return <div className='container'>
+    {
+      list.map((node) => <div key={node.id}>
+        <span>{node.name}</span>
+       { node.children && <List list={node.children} />}
+
+      </div>)
+    }
+  </div>
+
+}
+
+
 
 function App() {
+  const [data, setData] = useState(json);
 
   return (
-   <div>
-    <h1>VSCode-Sidebar/FileExplorer-Blueprint</h1>
-   </div>
+    <div>
+      <h1>VSCode-Sidebar/FileExplorer-Blueprint</h1>
+      <List list={data} />
+
+    </div>
   )
 }
 
