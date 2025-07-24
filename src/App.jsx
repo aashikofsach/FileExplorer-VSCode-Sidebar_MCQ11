37864b147eed004f2dbfd262a77cf9d88cf7f2df
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-24 17:39:06 +0530

    added the folder add icon corresponding to folder only

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,10 @@ function List({ list }) {
 
   return <div className='container'>
     {
-      list.map((node) => <div key={node.id}>
+      list.map((node) => <div key={node.id} className='parent' >
         {node.isFolder && <span onClick={() => setShow((prev) => ({...prev , [node.name] : !prev[node.name]}))}>{show?.[node.name] ? "-" : "+"} </span>}
         <span>{node.name}</span>
+       { node.isFolder && <span><img src="/add-folder.png" alt="add-folder" className='icon' /></span>}
 
         {show?.[node.name] && node.children && <List list={node.children} />}
 
