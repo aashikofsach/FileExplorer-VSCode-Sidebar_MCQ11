d903372e1d1e906afe10988387d35d856fbd1b45
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-24 19:34:38 +0530

    added the functionality for delete file and folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import json from "./../data.json";
 import { useState } from 'react';
 
 
-function List({ list ,addNodeToList}) {
+function List({ list ,addNodeToList ,deleteNodeFromlist}) {
   const [show, setShow] = useState({});
 
   return <div className='container'>
@@ -12,8 +12,9 @@ function List({ list ,addNodeToList}) {
         {node.isFolder && <span onClick={() => setShow((prev) => ({ ...prev, [node.name]: !prev[node.name] }))}>{show?.[node.name] ? "-" : "+"} </span>}
         <span>{node.name}</span>
         {node.isFolder && <span onClick={() => addNodeToList(node.id)}><img src="/add-folder.png" alt="add-folder" className='icon' /></span>}
+         {<span onClick={() => deleteNodeFromlist(node.id)}><img src="/delete-folder.png" alt="add-folder" className='icon2' /></span>}
 
-        {show?.[node.name] && node.children && <List list={node.children} addNodeToList={addNodeToList} />}
+        {show?.[node.name] && node.children && <List list={node.children} addNodeToList={addNodeToList} deleteNodeFromlist={deleteNodeFromlist} />}
 
       </div>)
     }
@@ -32,7 +33,7 @@ function App() {
       return list.map(node => {
         if (node.id === parentNode)
           return {
-            ...node, children: [...node.children, { id: 123, name: name, isFolder: true , children : [] }]
+            ...node, children: [...node.children, { id: Date.now().toString(), name: name, isFolder: true , children : [] }]
           }
           if(node.children)
           {
@@ -46,12 +47,27 @@ function App() {
     setData((prev) => updateTree(prev))
 
   }
+  const deleteNodeFromlist=(itemId)=>
+  {
+    const updateTree = (list)=>{
+      return list.filter((node)=> node.id!==itemId).map((node)=>{
+        if(node.children)
+        {
+          return {...node , children : updateTree(node.children)}
+        }
+        return node
+      })
+
+    }
+    setData((prev)=>updateTree(prev))
+
+  }
 
 
   return (
     <div>
       <h1>VSCode-Sidebar/FileExplorer-Blueprint</h1>
-      <List list={data} addNodeToList={addNodeToList} />
+      <List list={data} addNodeToList={addNodeToList} deleteNodeFromlist={deleteNodeFromlist} />
 
     </div>
   )
