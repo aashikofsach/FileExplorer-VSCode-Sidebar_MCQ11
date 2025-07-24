92415aaa7f23df1fe74570668cee961e6af3da06
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-24 19:07:51 +0530

    added new folder functionality using recursion and nested traversal omg

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import json from "./../data.json";
 import { useState } from 'react';
 
 
-function List({ list }) {
+function List({ list ,addNodeToList}) {
   const [show, setShow] = useState({});
 
   return <div className='container'>
     {
       list.map((node) => <div key={node.id} className='parent' >
-        {node.isFolder && <span onClick={() => setShow((prev) => ({...prev , [node.name] : !prev[node.name]}))}>{show?.[node.name] ? "-" : "+"} </span>}
+        {node.isFolder && <span onClick={() => setShow((prev) => ({ ...prev, [node.name]: !prev[node.name] }))}>{show?.[node.name] ? "-" : "+"} </span>}
         <span>{node.name}</span>
-       { node.isFolder && <span><img src="/add-folder.png" alt="add-folder" className='icon' /></span>}
+        {node.isFolder && <span onClick={() => addNodeToList(node.id)}><img src="/add-folder.png" alt="add-folder" className='icon' /></span>}
 
-        {show?.[node.name] && node.children && <List list={node.children} />}
+        {show?.[node.name] && node.children && <List list={node.children} addNodeToList={addNodeToList} />}
 
       </div>)
     }
@@ -26,10 +26,32 @@ function List({ list }) {
 function App() {
   const [data, setData] = useState(json);
 
+  const addNodeToList = (parentNode) => {
+    const name = prompt("enter name ")
+    const updateTree = (list) => {
+      return list.map(node => {
+        if (node.id === parentNode)
+          return {
+            ...node, children: [...node.children, { id: 123, name: name, isFolder: true , children : [] }]
+          }
+          if(node.children)
+          {
+           return {...node, children : updateTree(node.children)}
+          }
+          return node
+      })
+
+    };
+
+    setData((prev) => updateTree(prev))
+
+  }
+
+
   return (
     <div>
       <h1>VSCode-Sidebar/FileExplorer-Blueprint</h1>
-      <List list={data} />
+      <List list={data} addNodeToList={addNodeToList} />
 
     </div>
   )
